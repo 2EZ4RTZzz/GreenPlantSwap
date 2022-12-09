c9e1f6a884409bde38706f2864d590bc43f01598
2ez4rtz <98453275+2EZ4RTZzz@users.noreply.github.com>
2022-12-08 23:35:09 -0800

    339 P2 Company Website design

---
+++ Project2/js/script.js
@@ -0,0 +1,39 @@
+function changeImg(n) {
+  document.querySelector(".large-pic>img").src =
+    "../image/F" + n + ".png";
+}
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+function myFunction2() {
+  alert("Submitted");
+}
+
+function myFunction() {
+  alert("Thank you for following us");
+}
+
+function myFunction2() {
+  alert("Submitted");
+}
+
+function myFunction3() {
+  alert("Order Confirmed");
+}
+
+function myFunction4() {
+  alert("Successfully added to cart");
+}
+
+// hide the menus.
+function showMenu(){
+  // alert("123");
+  document.querySelector(".flowerNav").classList.toggle("showMenuStyle");
+  
+}
